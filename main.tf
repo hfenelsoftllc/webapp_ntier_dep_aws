01c7f6cd9d38f457f5bb9da7e5f6106578054e03
hfenelsoftllc <hfenelsoftllc@outlook.com>
2020-11-15 01:51:14 -0500

    Adding autoscaling modules layer to host the application

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+module "autoscaling" {
+  source = "./modules/autoscaling"
+  namespace = var.namespace
+  ssh_keypair = var.ssh_keypair
+
+  vpc = module.networking.vpc
+  sg = module.networking.sg
+  db_config = module.database.db_config
+}
+#step 2
 module "database" {
   source = "./modules/database"
   namespace = var.namespace
@@ -6,7 +16,7 @@ module "database" {
   sg = module.networking.sg
 }
 
-
+#step 1
 module "networking" {
   source    = "./modules/networking"
   namespace = var.namespace
